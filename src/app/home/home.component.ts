c59ef23db0512d73dc1b8dcf94c4af53142a2f87
n35k3nd1r <38220196+n35k3nd1r@users.noreply.github.com>
2023-03-01 01:07:42 +0100

    1 file modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { TranslateModule , TranslateService } from '@ngx-translate/core';
 
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    TranslateModule.forChild(),
+  ],
+})
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
